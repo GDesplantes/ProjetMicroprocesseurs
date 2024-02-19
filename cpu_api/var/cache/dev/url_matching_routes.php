ef72571b11eb2c72a79cb6be7bb2d952dba2e552
G.Desplantes <120396409+GDesplantes@users.noreply.github.com>
2024-02-19 11:15:26 +0100

    push

---
+++ cpu_api/var/cache/dev/url_matching_routes.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * This file has been auto-generated
+ * by the Symfony Routing Component.
+ */
+
+return [
+    false, // $matchHost
+    [ // $staticRoutes
+    ],
+    [ // $regexpList
+        0 => '{^(?'
+                .'|/api(?'
+                    .'|/\\.well\\-known/genid/([^/]++)(*:43)'
+                    .'|(?:/(index)(?:\\.([^/]++))?)?(*:78)'
+                    .'|/(?'
+                        .'|docs(?:\\.([^/]++))?(*:108)'
+                        .'|errors/([^/]++)(?'
+                            .'|(*:134)'
+                        .')'
+                        .'|validation_errors/([^/]++)(?'
+                            .'|(*:172)'
+                        .')'
+                        .'|cpus(?'
+                            .'|(?:\\.([^/]++))?(*:203)'
+                            .'|/([^/\\.]++)(?:\\.([^/]++))?(*:237)'
+                            .'|(?:\\.([^/]++))?(*:260)'
+                            .'|/([^/\\.]++)(?:\\.([^/]++))?(*:294)'
+                        .')'
+                    .')'
+                .')'
+                .'|/_error/(\\d+)(?:\\.([^/]++))?(*:333)'
+            .')/?$}sDu',
+    ],
+    [ // $dynamicRoutes
+        43 => [[['_route' => 'api_genid', '_controller' => 'api_platform.action.not_exposed', '_api_respond' => 'true'], ['id'], null, null, false, true, null]],
+        78 => [[['_route' => 'api_entrypoint', '_controller' => 'api_platform.action.entrypoint', '_format' => '', '_api_respond' => 'true', 'index' => 'index'], ['index', '_format'], null, null, false, true, null]],
+        108 => [[['_route' => 'api_doc', '_controller' => 'api_platform.action.documentation', '_format' => '', '_api_respond' => 'true'], ['_format'], null, null, false, true, null]],
+        134 => [
+            [['_route' => '_api_errors_problem', '_controller' => 'api_platform.symfony.main_controller', '_format' => null, '_stateless' => true, '_api_resource_class' => 'ApiPlatform\\State\\ApiResource\\Error', '_api_operation_name' => '_api_errors_problem'], ['status'], ['GET' => 0], null, false, true, null],
+            [['_route' => '_api_errors_hydra', '_controller' => 'api_platform.symfony.main_controller', '_format' => null, '_stateless' => true, '_api_resource_class' => 'ApiPlatform\\State\\ApiResource\\Error', '_api_operation_name' => '_api_errors_hydra'], ['status'], ['GET' => 0], null, false, true, null],
+            [['_route' => '_api_errors_jsonapi', '_controller' => 'api_platform.symfony.main_controller', '_format' => null, '_stateless' => true, '_api_resource_class' => 'ApiPlatform\\State\\ApiResource\\Error', '_api_operation_name' => '_api_errors_jsonapi'], ['status'], ['GET' => 0], null, false, true, null],
+        ],
+        172 => [
+            [['_route' => '_api_validation_errors_problem', '_controller' => 'api_platform.symfony.main_controller', '_format' => null, '_stateless' => true, '_api_resource_class' => 'ApiPlatform\\Symfony\\Validator\\Exception\\ValidationException', '_api_operation_name' => '_api_validation_errors_problem'], ['id'], ['GET' => 0], null, false, true, null],
+            [['_route' => '_api_validation_errors_hydra', '_controller' => 'api_platform.symfony.main_controller', '_format' => null, '_stateless' => true, '_api_resource_class' => 'ApiPlatform\\Symfony\\Validator\\Exception\\ValidationException', '_api_operation_name' => '_api_validation_errors_hydra'], ['id'], ['GET' => 0], null, false, true, null],
+            [['_route' => '_api_validation_errors_jsonapi', '_controller' => 'api_platform.symfony.main_controller', '_format' => null, '_stateless' => true, '_api_resource_class' => 'ApiPlatform\\Symfony\\Validator\\Exception\\ValidationException', '_api_operation_name' => '_api_validation_errors_jsonapi'], ['id'], ['GET' => 0], null, false, true, null],
+        ],
+        203 => [[['_route' => '_api_/cpus{._format}_get_collection', '_controller' => 'api_platform.symfony.main_controller', '_format' => null, '_stateless' => true, '_api_resource_class' => 'App\\Entity\\CPU', '_api_operation_name' => '_api_/cpus{._format}_get_collection'], ['_format'], ['GET' => 0], null, false, true, null]],
+        237 => [[['_route' => '_api_/cpus/{id}{._format}_get', '_controller' => 'api_platform.symfony.main_controller', '_format' => null, '_stateless' => true, '_api_resource_class' => 'App\\Entity\\CPU', '_api_operation_name' => '_api_/cpus/{id}{._format}_get'], ['id', '_format'], ['GET' => 0], null, false, true, null]],
+        260 => [[['_route' => '_api_/cpus{._format}_post', '_controller' => 'api_platform.symfony.main_controller', '_format' => null, '_stateless' => true, '_api_resource_class' => 'App\\Entity\\CPU', '_api_operation_name' => '_api_/cpus{._format}_post'], ['_format'], ['POST' => 0], null, false, true, null]],
+        294 => [[['_route' => '_api_/cpus/{id}{._format}_put', '_controller' => 'api_platform.symfony.main_controller', '_format' => null, '_stateless' => true, '_api_resource_class' => 'App\\Entity\\CPU', '_api_operation_name' => '_api_/cpus/{id}{._format}_put'], ['id', '_format'], ['PUT' => 0], null, false, true, null]],
+        333 => [
+            [['_route' => '_preview_error', '_controller' => 'error_controller::preview', '_format' => 'html'], ['code', '_format'], null, null, false, true, null],
+            [null, null, null, null, false, false, 0],
+        ],
+    ],
+    null, // $checkCondition
+];
